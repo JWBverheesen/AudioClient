6532f21bb2250091fe1e58117126e5491f5d29ae
JWBVerheesen <j.verheesen@hotmail.nl>
2026-09-07 21:26:20 +0200

    use own MaterialTheme and default to dark

--- app/src/main/java/com/example/audioclient/MainActivity.kt
+++ app/src/main/java/com/example/audioclient/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.example.audioclient.ui.AudioClientApp
+import com.example.audioclient.ui.theme.AudioClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            AudioClientTheme {
                 Surface {
                     AudioClientApp()
                 }
